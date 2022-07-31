f125be66e94fa47a6b68756465606d10da7178bc
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-07-31 20:09:13 +0200

    API Lista

--- src/App.jsx
+++ src/App.jsx
@@ -24,3 +24,12 @@ export const App = () => {
   return <div>Hola Mundo</div>;
 }
 */
+
+/*Si usas sintaxis 1, cuando importes este componente en otra vista,
+puedes llamar al componente con el nombre que te de la gana, y especificando la ruta. 
+El tema es que la idea es que llames a los componentes por su nombre.
+Ejemplo:
+import logo from "../component/img/traveland.png" 
+En cambio, usando sintaxis 2 o 3, la importación debe ser por su nombre, de la siguiente manera:
+import { Trip } from "./pages/trip.jsx";
+*/
--- src/services/launches.js
+++ src/services/launches.js
@@ -10,6 +10,20 @@ export const getAllLaunches = async () => {
   }
 };
 
+export const getLaunchByFlightNumber = async (flightNumber) => {
+    try {
+      const response = await fetch(API_URL + "/launches/" + flightNumber);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+/* Petición fetch de tipo GET. Librerias para fetching:
+-Axios
+-Fetching es la librería nativa */
+
 // export async function getAllLaunches() {
 //   const response = await fetch(API_URL + "/launches");
 //   const data = await response.json();
