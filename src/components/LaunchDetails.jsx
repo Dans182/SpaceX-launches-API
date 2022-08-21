6034094e3dee44f2fed791f1ae18c2c128f73140
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-08-21 12:23:28 +0200

    estilos y embed videos

--- src/components/LaunchDetails.jsx
+++ src/components/LaunchDetails.jsx
@@ -1,7 +1,15 @@
 import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import * as API from "../services/launches";
-import { Box, Flex, Text, Spacer, Tag } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Spacer,
+  Tag,
+  Image,
+  AspectRatio,
+} from "@chakra-ui/react";
 import format from "date-fns/format";
 import { parseISO } from "date-fns";
 
@@ -18,25 +26,60 @@ export const LaunchDetails = () => {
   return (
     <Fragment>
       <Box bg="gray.100" padding={4} margin={4} borderRadius="lg">
-        {!launch ? (
-          <div>Loading...</div>
-        ) : (
-          <Box bg="gray.100" padding={4} margin={4} borderRadius="lg">
+        {launch && launch.links && launch.links.patch.small ? (
+          <Box
+            bg="gray.100"
+            minW="540px"
+            padding={4}
+            margin={4}
+            borderRadius="lg"
+          >
             <Flex>
+              <Image
+                src={launch.links.patch.small}
+                width={150}
+                margin={4}
+              ></Image>
+
               <Text fontSize="2x1">
                 Mission:{" "}
                 <strong>
-                  {launch.name} 
-                  {/* ({format(parseISO(launch.date_utc), "yyyy")}) */}
+                  {launch.name} ({format(parseISO(launch.date_utc), "yyyy")})
+                </strong>
+                <Spacer />
+                Flight number: <strong>{launch.flight_number}</strong>
+                <Spacer />
+                Flight date:{" "}
+                <strong>
+                  {format(parseISO(launch.date_utc), "dd/MM/yyyy")}
                 </strong>
               </Text>
               <Spacer />
-              <Tag padding={2} colorScheme={launch.success ? "green" : "red"}>
+              <Tag
+                height={50}
+                padding={2}
+                colorScheme={launch.success ? "green" : "red"}
+              >
                 {launch.success ? "Success" : "Failure"}
               </Tag>
             </Flex>
-            <Box></Box>
+            <br />
+            <Text fontSize="2x1">{launch.details}</Text>
+            <br />
+            <Box>
+              <AspectRatio maxW="560px" mx="auto" ratio={1}>
+                <iframe
+                  title={launch.name}
+                  src={
+                    "https://www.youtube.com/embed/" + launch.links.youtube_id
+                  }
+                  allowFullScreen
+                />
+              </AspectRatio>
+            </Box>
           </Box>
+        ) : (
+          <div>Loading...</div>
         )}
       </Box>
     </Fragment>
